b89453a67b992c6ccd964586cb008ef69a067aa9
BMJ-Coughlan <brendan.m.coughlan@gmail.com>
2025-11-26 19:47:33 +0000

    feat: add Prettier configuration and update Playwright reporter formatting

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,10 +15,9 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 2 : 8,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter:
-    process.env.CI
-      ? [['github'], ['html', { open: 'never' }], ['list']]
-      : [['html', { open: 'always' }], ['list']],
+  reporter: process.env.CI
+    ? [['github'], ['html', { open: 'never' }], ['list']]
+    : [['html', { open: 'always' }], ['list']],
   use: {
     baseURL: 'https://practice.expandtesting.com',
 
